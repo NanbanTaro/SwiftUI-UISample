c9e02a8935ee44af171e5ab511f280ea2762faae
NanbanTaro <>
2023-12-24 19:34:01 +0900

    新規プロジェクト作成

---
+++ SwiftUISampleProject/SwiftUISampleProjectApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUISampleProjectApp.swift
+//  SwiftUISampleProject
+//
+//  Created by NanbanTaro on 2023/12/10.
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
