38d449e91dfad6b95ed687e91b25500916a91b23
NanbanTaro <chicken.de.nanban@gmail.com>
2025-06-15 21:10:05 +0900

    スクロール時のアニメーションView追加

---
+++ SwiftUISampleProject/ViewSample/Scroll/ScrollAnimationItem.swift
@@ -0,0 +1,53 @@
+//
+//  ScrollAnimationItem.swift
+//  SwiftUISampleProject
+//
+//  Created by NanbanTaro on 2025/06/15.
+//  
+//
+
+import SwiftUI
+
+/// スクロール表示時のアニメーションアイテム
+struct ScrollAnimationItem: View {
+    /// パレット
+    let pallet: ColorPallet
+
+    /// 表示フラグ
+    @State private var isShowing = false
+
+    // MARK: - body
+
+    var body: some View {
+        GeometryReader { geometry in
+            if #available(iOS 18.0, *) {
+                HStack(spacing: 0) {
+                    ForEach(pallet.colors, id: \.self) {color in
+                        color
+                            .offset(
+                                x: 0 ,
+                                y: isShowing ? 0 : geometry.size.height
+                            )
+                    }
+                }
+                .onScrollVisibilityChange(threshold: 0.9) {_ in
+                    guard !isShowing else { return }
+
+                    withAnimation(.easeOut(duration: 1)) {
+                        isShowing = true
+                    }
+                }
+            } else {
+                HStack(spacing: 0) {
+                    ForEach(pallet.colors, id: \.self) { color in
+                        color
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollAnimationItem(pallet: ColorPallet())
+}
